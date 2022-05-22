35ac8aee1b84ad21a52dc3364902762166b184d7
thayeylolu <hamdallahhassanah@gmail.com>
2022-05-21 17:44:43 -0700

    feat: remove duplicate profile

--- react-revenue-dashboard-master/src/components/MainContent.jsx
+++ react-revenue-dashboard-master/src/components/MainContent.jsx
@@ -12,14 +12,6 @@ function MainContent() {
       <SubContainer>
         <SectionOne>
             <Profile/>
-            <Profile/>
-            <Profile/>
-        </SectionOne>
-
-        <SectionOne>
-            <Profile/>
-            <Profile/>
-            <Profile/>
         </SectionOne>
       </SubContainer>
     </Container>
@@ -27,55 +19,58 @@ function MainContent() {
 }
 
 const Container = styled.div`
-  width: 90%;
+  width: 100% !important;
+  
   background: linear-gradient(to bottom right, #e6e4ff 0%, thistle 70%);
-  margin: 1rem 1rem 1rem 1rem;
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
+  //margin: 1rem 1rem 1rem 1rem;
+  @media screen and (min-width: 320px) and (max-width: 560px) {
     display: flex;
     flex-direction: column;
-    width: 100%;
-    margin: 1rem;
-    padding 1rem;
+    // margin: 1rem;
+    // padding 1rem;
   }
 `;
 
 const SubContainer = styled.div`
   //margin: 0.5rem 0.5rem 0.5rem 0.5rem;
-  margin-top:-3rem;
-  margin-left:-1rem;
-  marign-right:1rem;
-  //padding: 1rem 1rem 1rem 1rem;
-  height: 60%;
-  width: 100%;
+  // margin-top:-3rem;
+  // margin-left:-1rem;
+  // marign-right:1rem;
+  padding: 1rem 1rem 1rem 2rem;
+  // height: 60%;
+  width: 80%;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  justify-content: center;
+  margin: 0 auto;
+  
+  justify-content: space-around;
   @media screen and (min-width: 320px) and (max-width: 1080px) {
     height: 100%;
   }
 `;
 const SectionOne = styled.div`
   display: flex;
-  //justify-content: space-between; 
-  height: 40%;
+  justify-content: space-evenly; 
+  flex-wrap: wrap;
+  // height: 40%;
   gap: 2rem;
   padding-top: 0rem;
-  margin: 2.5rem 3rem 3rem 3rem;
+  justify-content: space-evenly;
+  //margin: 2.5rem 3rem 3rem 3rem;
   width: 100%;
   @media screen and (min-width: 320px) and (max-width: 1080px) {
     flex-direction: column;
     align-items: center;
-    height: max-content;
+    //height: max-content;
   }
 `;
 const ColumnOne1 = styled.div`
   display: flex;
   gap: 1rem;
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
+  @media screen and (min-width: 320px) and (max-width: 560px) {
     flex-direction: column;
     //justify-content: center;
-    align-items: center;
-    gap: 1rem;
     width: 100%;
   }
 `;
@@ -86,7 +81,7 @@ const ColumnTwo1 = styled.div`
   height: 115%;
   width: 100%;
   @media screen and (min-width: 320px) and (max-width: 1080px) {
-    height: max-content;
+    //height: max-content;
     justify-content: center;
     align-items: center;
   }
