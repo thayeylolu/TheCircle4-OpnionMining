51955d7c042647916b6ab7d0b31f094bd4a396b9
thayeylolu <hamdallahhassanah@gmail.com>
2022-06-03 21:41:05 -0700

    feat:style for dash app

---
+++ app/components/style.py
@@ -0,0 +1,36 @@
+def BgStyle():
+    return {
+        "display": "flex",
+        "width": "100%",
+        "height": "95vh",
+        "padding": "auto",
+        "margin": "auto",
+        "background": "F5F5F5",
+        "border": "1px solid #161414",
+    }
+
+
+def RightContainer():
+    return {
+        
+        "display": "flex",
+        "flex": "1",
+        "align-items": "center",
+        "flex-direction": "row",
+        "background": "#F5F5F5",
+        "border": "1px solid #000000",
+        "width": "20%",
+    }
+
+
+def LeftContainer():
+    return {
+     
+        "display": "flex",
+        "flex": "1",
+        "flex-direction": "row",
+        "align-items": "center",
+        "background": "#F5F5F5",
+        "border": "1px solid #000000",
+        "width": "50%",
+    }
